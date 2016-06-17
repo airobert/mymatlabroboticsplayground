cc2e831e7c2a54253e8152a7b16e36ef0405f24b
Robert White <ai.robert.wangshuai@gmail.com>
2016-06-17 16:39:57 +0200

    part 1 working

--- week2/triangle_intersection.m
+++ week2/triangle_intersection.m
@@ -16,26 +16,37 @@ min_x_2 = min(P2(:,1));
 max_y_2 = max(P2(:,2));
 min_y_2 = min(P2(:,2));
 
+
 flag = true;
+
 
 if (min_x_1 > max_x_2 || min_x_2 > max_x_1 || min_y_1 > max_y_2 || min_y_2 > max_y_1 )
     flag = false;
 else
     %they may have intersections
-    
+  
+
     for A = P1'
         for B = P1'
             for C = P1'
                 if (~(isequal(A, B) || isequal(A, C) || isequal(C, B)))
-                    m = (A(2) - B(2)) / (A(1) - B(1));
-                    b = A(2) - m * A(1);
-                    
                     A_ = P2(1,:);
                     B_ = P2(2,:);
                     C_ = P2(3,:);
                     
-                    if ((m*A_(1) - A_(2) + b > 0)  && (m*B_(1) - B_(2) + b > 0) && (m*C_(1) - C_(2) + b > 0) && (m*C(1) - C(2) + b < 0))
-                        flag = false;
+                    if (A(1) - B(1) == 0)
+                        % then the equation is x = A(1)
+                        if ((A_(1) > A(1) && B_(1) > A(1) && C_(1) > A(1) && C(1) < A(1)) || (A_(1) < A(1) && B_(1) < A(1) && C_(1) < A(1) && C(1) > A(1)))
+                            flag = false;
+                        end
+                    else
+                            
+                        m = (A(2) - B(2)) / (A(1) - B(1));
+                        b = A(2) - m * A(1);
+                    
+                        if (((m*A_(1) - A_(2) + b > 0)  && (m*B_(1) - B_(2) + b > 0) && (m*C_(1) - C_(2) + b > 0) && (m*C(1) - C(2) + b < 0)) || ((m*A_(1) - A_(2) + b < 0)  && (m*B_(1) - B_(2) + b < 0) && (m*C_(1) - C_(2) + b < 0) && (m*C(1) - C(2) + b > 0)))
+                            flag = false;
+                        end
                     end
                 end
             end
@@ -46,15 +57,23 @@ else
         for B = P2'
             for C = P2'
                 if (~(isequal(A, B) || isequal(A, C) || isequal(C, B)))
-                    m = (A(2) - B(2)) / (A(1) - B(1));
-                    b = A(2) - m * A(1);
-                    
                     A_ = P1(1,:);
                     B_ = P1(2,:);
                     C_ = P1(3,:);
+                      
+                    if (A(1) - B(1) == 0)
+                        % then the equation is x = A(1)
+                        if ((A_(1) > A(1) && B_(1) > A(1) && C_(1) > A(1) && C(1) < A(1)) || (A_(1) < A(1) && B_(1) < A(1) && C_(1) < A(1) && C(1) > A(1)))
+                            flag = false;
+                        end
+                    else
                     
-                    if ((m*A_(1) - A_(2) + b > 0)  && (m*B_(1) - B_(2) + b > 0) && (m*C_(1) - C_(2) + b > 0) && (m*C(1) - C(2) + b < 0))
-                        flag = false;
+                        m = (A(2) - B(2)) / (A(1) - B(1));
+                        b = A(2) - m * A(1);
+
+                        if (((m*A_(1) - A_(2) + b > 0)  && (m*B_(1) - B_(2) + b > 0) && (m*C_(1) - C_(2) + b > 0) && (m*C(1) - C(2) + b < 0)) || ((m*A_(1) - A_(2) + b < 0)  && (m*B_(1) - B_(2) + b < 0) && (m*C_(1) - C_(2) + b < 0) && (m*C(1) - C(2) + b > 0)))
+                            flag = false;
+                        end
                     end
                 end
             end
