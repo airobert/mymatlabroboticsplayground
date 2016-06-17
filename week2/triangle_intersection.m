230a496d05e2e4aa163f2a13683be37afdd77964
Robert White <ai.robert.wangshuai@gmail.com>
2016-06-17 11:08:00 +0200

    git ignore

--- week2/triangle_intersection.m
+++ week2/triangle_intersection.m
@@ -4,6 +4,34 @@ function flag = triangle_intersection(P1, P2)
 %%% All of your code should be between the two lines of stars.
 % *******************************************************************
 
-flag = false;
+max_x_1 = max(P1(:,1));
+min_x_1 = min(P1(:,1));
+
+max_y_1 = max(P1(:,2));
+min_y_1 = min(P1(:,2));
+
+max_x_2 = max(P2(:,1));
+min_x_2 = min(P2(:,1));
+
+max_y_2 = max(P2(:,2));
+min_y_2 = min(P2(:,2));
+
+if (min_x_1 > max_x_2 || min_x_2 > max_x_1 || min_y_1 > max_y_2 || min_y_2 > max_y_1 )
+    flag = false;
+else
+    %they may have intersections
+    
+    %
+    a = two_sides (P1(1,:), P1(2,:), P1(3,:), P2(1,:), P2(2,:), P2(3,:));
+    b = two_sides (P1(3,:), P1(2,:), P1(3,:), P2(1,:), P2(2,:), P2(3,:));
+    
+    if (a || b)
+        flag = false;  % if one of the edge of one of the 
+    else
+        flag = true;
+    end
+          
+end
 % *******************************************************************
 end
+
