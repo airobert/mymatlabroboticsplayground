745c9e5135a974a04bd982e5e2c560e1181b4941
Robert White <ai.robert.wangshuai@gmail.com>
2016-06-17 16:01:10 +0200

    still not so sure

--- week2/triangle_intersection.m
+++ week2/triangle_intersection.m
@@ -16,19 +16,49 @@ min_x_2 = min(P2(:,1));
 max_y_2 = max(P2(:,2));
 min_y_2 = min(P2(:,2));
 
+flag = true;
+
 if (min_x_1 > max_x_2 || min_x_2 > max_x_1 || min_y_1 > max_y_2 || min_y_2 > max_y_1 )
     flag = false;
 else
     %they may have intersections
     
-    %
-    a = two_sides (P1(1,:), P1(2,:), P1(3,:), P2(1,:), P2(2,:), P2(3,:));
-    b = two_sides (P1(3,:), P1(2,:), P1(3,:), P2(1,:), P2(2,:), P2(3,:));
+    for A = P1'
+        for B = P1'
+            for C = P1'
+                if (~(isequal(A, B) || isequal(A, C) || isequal(C, B)))
+                    m = (A(2) - B(2)) / (A(1) - B(1));
+                    b = A(2) - m * A(1);
+                    
+                    A_ = P2(1,:);
+                    B_ = P2(2,:);
+                    C_ = P2(3,:);
+                    
+                    if ((m*A_(1) - A_(2) + b > 0)  && (m*B_(1) - B_(2) + b > 0) && (m*C_(1) - C_(2) + b > 0) && (m*C(1) - C(2) + b < 0))
+                        flag = false;
+                    end
+                end
+            end
+        end
+    end
     
-    if (a || b)
-        flag = false;  % if one of the edge of one of the 
-    else
-        flag = true;
+    for A = P2'
+        for B = P2'
+            for C = P2'
+                if (~(isequal(A, B) || isequal(A, C) || isequal(C, B)))
+                    m = (A(2) - B(2)) / (A(1) - B(1));
+                    b = A(2) - m * A(1);
+                    
+                    A_ = P1(1,:);
+                    B_ = P1(2,:);
+                    C_ = P1(3,:);
+                    
+                    if ((m*A_(1) - A_(2) + b > 0)  && (m*B_(1) - B_(2) + b > 0) && (m*C_(1) - C_(2) + b > 0) && (m*C(1) - C(2) + b < 0))
+                        flag = false;
+                    end
+                end
+            end
+        end
     end
           
 end
